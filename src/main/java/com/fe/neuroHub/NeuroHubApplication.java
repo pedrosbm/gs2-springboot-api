88218177065ee842fff89f233b5740eaf8790471
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2023-11-15 17:57:04 -0300

    projeto springboot primeiro commit

---
+++ src/main/java/com/fe/neuroHub/NeuroHubApplication.java
@@ -0,0 +1,13 @@
+package com.fe.neuroHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroHubApplication.class, args);
+	}
+
+}
